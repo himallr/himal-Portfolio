008a723c3302316afe9b9eb9f726c5866fd891b6
HIMAL L R <117270050+himallr@users.noreply.github.com>
2024-01-13 18:14:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
